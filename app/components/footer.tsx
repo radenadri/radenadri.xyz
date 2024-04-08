382af970f894c4dd12aa9c2e20a0a01e545dd5a7
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-04-09 05:02:58 +0700

    fix: link in footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,10 +35,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/radenadri.xyz"
+            href="https://github.com/radenadri/radenadri.xyz"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7">this site repo</p>
           </a>
         </li>
         <li>
