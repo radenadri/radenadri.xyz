c7120edd17cd90ae6812781052ea198c8bae39e3
Adriana Eka Prayudha <radenadriep@gmail.com>
2025-12-01 15:42:19 +0700

    feat: new content

--- src/data/works.ts
+++ src/data/works.ts
@@ -1,12 +1,11 @@
 export default [{
-  title: 'Pos Properti Payment processor',
-  slug: 'pos-properti-payment-processor',
-  description: 'A web-based application created to be a payment processor for all transactions from PT Pos Properti Indonesia internal system, integrated with one of the largest local banks in Indonesia.',
-  coverImage: '/viz/pospro-cover.png',
-},
-{
   title: 'Passport',
   slug: 'passport',
   description: 'A mobile-based HRIS application created last year to support daily activities for internal operations in the office.',
   coverImage: '/viz/passport-cover.png',
-}]
+}, {
+  title: 'Whatsapp Link Generator',
+  slug: 'whatsapp-link-generator',
+  description: 'A web-based application created to generate link for whatsapp.',
+  coverImage: '/viz/whatsapp-link-generator-cover.png',
+},]
