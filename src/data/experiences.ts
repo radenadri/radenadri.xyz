ccaea5fa6b7131f07ea0acb28840a2b62f70cfcf
Adriana Eka Prayudha <radenadriep@gmail.com>
2026-03-04 12:37:13 +0700

    feat: modify works

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,25 +1,27 @@
-export default [{
-  company: 'Self Employed',
-  position: 'Freelance Fullstack Engineer',
-  links: '#',
-  location: 'Bandung, Indonesia',
-  duration: 'September 2025 - Now',
-  jobDescription: [
-    'Develop websites and applications with responsive designs and enhance features using the latest technologies.',
-    'Design and optimize database schemas, including query tuning, to improve application performance.',
-    'Implement full-stack solutions using Laravel and React for application development needs.',
-  ],
-}, {
-  company: 'PT. Pentacode Digital',
-  position: 'Full Stack PHP Developer',
-  links: 'https://pentacode.id',
-  location: 'Bandung, Indonesia',
-  duration: 'August 2022 - September 2025',
-  jobDescription: [
-    'Build and develop responsive web interfaces using Wordpress, Laravel and React including features for e-commerce, HR, inventory, and payment dashboards.',
-    'Build a backend and also build a web services (REST API) to be integrated with the frontend, also collaborate with backend developers for API integration, feature enhancements, and ensuring performance and cross-device compatibility.',
-    'Writing code documentation, building application schema diagram and also ERD or any application flow that needed.',
-    'Write clean, structured, and maintainable code using modern practices',
-    'Use Git for version control, debugging, and ensuring an optimal user experience through responsive design and frontend optimization.',
-  ],
-}]
+export default [
+  {
+    company: "Self Employed",
+    position: "Fullstack Developer",
+    links: "#",
+    location: "Bandung, Indonesia",
+    duration: "Oct 2025 - Now",
+    jobDescription: [
+      "Develop solutions and helping people reach their goals in technology needs",
+      "Design and optimize database schemas, including query tuning, to improve application performance.",
+      "Implement full-stack solutions using Laravel and React for web applications and Flutter / React Native for mobile apps",
+    ],
+  },
+  {
+    company: "PT. Pentacode Digital",
+    position: "Fullstack Developer",
+    links: "https://pentacode.id",
+    location: "Bandung, Indonesia",
+    duration: "August 2022 - September 2025",
+    jobDescription: [
+      "Build and develop web apps using Laravel and React including features for e-commerce, HR, inventory, and payment dashboards.",
+      "Build and develop cross platform mobile application (Android & iOS) using Flutter and React Native",
+      "Write clean, structured, and maintainable code using modern practices",
+      "Use Git for version control",
+    ],
+  },
+];
